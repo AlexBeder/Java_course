9194cdaaee11a067f03f03b3adbd479e596a1c85
Ruilcaus <beder.alex@gmail.com>
2019-10-16 22:06:05 -0400

    Git unregisterred

--- sandbox/src/main/java/package_lecture1p1/HelloWorld.java
+++ sandbox/src/main/java/package_lecture1p1/HelloWorld.java
@@ -2,8 +2,8 @@ package package_lecture1p1;
 
 public class HelloWorld {
 
-public static void main(String[] args) {
-System.out.println("Hello World");
+  public static void main(String[] args) {
+    System.out.println("Hello World");
 
-}
+  }
 }
