5b7ef8d756aa575ceb208709e0199d712ac455cd
Ruilcaus <beder.alex@gmail.com>
2019-10-22 20:45:22 -0400

    Test for Group Creation

---
+++ addressbook-web-tests/src/main/java/new_package/GroupData.java
@@ -0,0 +1,14 @@
+package new_package;
+
+public class GroupData {
+  public String GroupName;
+  public String GroupHeader;
+  public String GroupFooter;
+
+  public GroupData (String GroupName, String GroupHeader, String GroupFooter){
+    this.GroupName = GroupName;
+    this.GroupHeader = GroupHeader;
+    this.GroupFooter = GroupFooter;
+  }
+
+}
