f8ca4fc40df5aef1b876590d276ceb0734e4a176
Ruilcaus <beder.alex@gmail.com>
2019-10-26 23:06:51 -0400

    Cross Browser testing is added

--- addressbook-web-tests/src/test/java/new_package/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/new_package/tests/TestBase.java
@@ -1,12 +1,13 @@
 package new_package.tests;
 
 import new_package.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 public class TestBase {
 
-  protected final ApplicationManager app = new ApplicationManager();
+  protected final ApplicationManager app = new ApplicationManager(BrowserType.EDGE);
 
 
   @BeforeMethod(alwaysRun = true)
