b6606ff71a35d0959f0b35f11703ad9a20fcb7dc
Ruilcaus <beder.alex@gmail.com>
2019-11-03 19:55:07 -0500

    Primes lesson 4.1

--- sandbox/src/test/java/java_practice/PrimeTests.java
+++ sandbox/src/test/java/java_practice/PrimeTests.java
@@ -8,6 +8,15 @@ public class PrimeTests {
   public void testPrime() {
     Assert.assertTrue(Primes.isPrime(Integer.MAX_VALUE));
   }
+  @Test
+  public void testPrimeFast() {
+    Assert.assertTrue(Primes.isPrimeFast(Integer.MAX_VALUE));
+  }
+  @Test (enabled = false)
+  public void testPrimeLong() {
+    long n = Integer.MAX_VALUE;
+    Assert.assertTrue(Primes.isPrime(n));
+  }
 
   @Test
   public void testNonPrime() {
