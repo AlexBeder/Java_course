20cb0b414ea301542e70708127e863f6884e21d7
Ruilcaus <beder.alex@gmail.com>
2019-11-04 20:24:23 -0500

    lesson 4.3 - count of groups

--- addressbook-web-tests/src/test/java/new_package/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/new_package/tests/GroupCreationTests.java
@@ -13,7 +13,7 @@ public class GroupCreationTests extends TestBase {
     int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().createGroup(new GroupData("Test1", "testik2", "Testik3"));
     int after = app.getGroupHelper().getGroupCount();
-    Assert.assertEquals(after, before+1);
+    Assert.assertEquals(after, before + 1);
     app.logout();
 
   }
--- addressbook-web-tests/src/test/java/new_package/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/new_package/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package new_package.tests;
 
 import new_package.model.GroupData;
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 public class GroupDeletionTests extends TestBase {
@@ -11,9 +12,12 @@ public class GroupDeletionTests extends TestBase {
     if (! app.getGroupHelper().isThereARecord()){
       app.getGroupHelper().createGroup(new GroupData("Test1", "testik2", "Testik3"));
     }
+    int before = app.getGroupHelper().getGroupCount();
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().clickDeleteButton();
     app.getGroupHelper().returnToGroupPage();
+    int after = app.getGroupHelper().getGroupCount();
+    Assert.assertEquals(after, before - 1);
   }
 
 }
