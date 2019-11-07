cdbd15742b59b4613a826df2ef4975f0c6a37ec9
Ruilcaus <beder.alex@gmail.com>
2019-11-06 21:10:33 -0500

    lesson 4.9 lambda for group and contact creations

--- addressbook-web-tests/src/test/java/new_package/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/new_package/tests/ContactCreationTests.java
@@ -5,6 +5,7 @@ import new_package.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -21,13 +22,7 @@ public class ContactCreationTests extends TestBase {
     List<ContactData> after = app.getContactHelper().getContactList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (ContactData c : after){
-      if (c.getId() > max){
-        max= c.getId();
-      }
-    }
-    contact.setId(max);
+    contact.setId(after.stream().max((Comparator<ContactData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object> (after));
 
--- addressbook-web-tests/src/test/java/new_package/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/new_package/tests/GroupCreationTests.java
@@ -2,8 +2,9 @@ package new_package.tests;
 
 import new_package.model.GroupData;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.Test;
 
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 
@@ -15,18 +16,12 @@ public class GroupCreationTests extends TestBase {
     app.getNavigationHelper().gotoGroupPage();
     List<GroupData> before = app.getGroupHelper().getGroupList();
 
-    GroupData group = new GroupData("Test1", "testik2", "Testik3");
+    GroupData group = new GroupData("Test11", "testik2", "Testik3");
     app.getGroupHelper().createGroup(group);
     List<GroupData> after = app.getGroupHelper().getGroupList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (GroupData g : after){
-      if (g.getId() > max){
-        max= g.getId();
-      }
-    }
-    group.setId(max);
+    group.setId(after.stream().max((Comparator<GroupData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object> (after));
 
