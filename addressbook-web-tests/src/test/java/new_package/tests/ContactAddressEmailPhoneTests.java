d32597eaa0049949a179d850ec079047b6a405ea
Ruilcaus <beder.alex@gmail.com>
2019-11-18 20:10:17 -0500

    exercise 11

--- addressbook-web-tests/src/test/java/new_package/tests/ContactAddressEmailPhoneTests.java
+++ addressbook-web-tests/src/test/java/new_package/tests/ContactAddressEmailPhoneTests.java
@@ -10,7 +10,7 @@ import java.util.stream.Collectors;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-public class ContactEmailTests extends TestBase {
+public class ContactAddressEmailPhoneTests extends TestBase {
   @BeforeMethod
   public void ensurePreconditions(){
     if (app.contact().list().size() == 0) {
@@ -20,17 +20,29 @@ public class ContactEmailTests extends TestBase {
   }
 
 @Test
-  public void testContactEmails(){
+  public void testContactPhones(){
   app.contact().gotoHomePage();
   ContactData contact = app.contact().all().iterator().next();
   ContactData contactInfoFromEditForm = app.contact().infoFromEditForm(contact);
 
+  assertThat(contact.getAddress(), equalTo(contactInfoFromEditForm.getAddress()));
   assertThat(contact.getAllEmails(), equalTo(mergeEmails(contactInfoFromEditForm)));
+  assertThat(contact.getAllPhones(), equalTo(mergePhones(contactInfoFromEditForm)));
+}
+
+  private String mergePhones(ContactData contact) {
+    return Arrays.asList(contact.getHomePhone(), contact.getMobilePhone(), contact.getWorkPhone())
+            .stream().filter((s) -> ! s.equals(""))
+            .map(ContactAddressEmailPhoneTests::cleaned)
+            .collect(Collectors.joining("\n"));
+
   }
   private String mergeEmails(ContactData contact) {
     return Arrays.asList(contact.getEmail(), contact.getEmail2(), contact.getEmail3())
             .stream().filter((s) -> ! s.equals("")).collect(Collectors.joining("\n"));
 
   }
-
+  public static String cleaned(String phone){
+  return phone.replaceAll("\\s","").replaceAll("[-()]","");
+}
 }
