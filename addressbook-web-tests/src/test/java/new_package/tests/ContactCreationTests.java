9f0f37d5bbd2b375cb7060bac46518d5d1cd81a3
Ruilcaus <beder.alex@gmail.com>
2019-11-20 22:17:48 -0500

    lesson 6.4 group + contacts

--- addressbook-web-tests/src/test/java/new_package/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/new_package/tests/ContactCreationTests.java
@@ -2,23 +2,36 @@ package new_package.tests;
 
 import new_package.model.ContactData;
 import new_package.model.Contacts;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBase {
+  @DataProvider
+  public Iterator<Object[]> validContacts(){
+    List<Object[]> list = new ArrayList<Object[]>();
+    File photo = new File("src/test/resources/stru.png");
+    list.add(new Object[] {new ContactData().withFirstName("Name 1").withLastName("Familiya 1")
+            .withPhoto(photo).withAddress("Adres 1").withHomeTel("704").withEmail("@gmail.com").withGroup("[none]")});
+    list.add(new Object[] {new ContactData().withFirstName("Name 2").withLastName("Familiya 2")
+            .withPhoto(photo).withAddress("Adres 2").withHomeTel("704").withEmail("@gmail.com").withGroup("[none]")});
+    list.add(new Object[] {new ContactData().withFirstName("Name 3").withLastName("Familiya 3")
+            .withPhoto(photo).withAddress("Adres 3").withHomeTel("704").withEmail("@gmail.com").withGroup("[none]")});
+    return list.iterator();
+  }
 
-  @Test
-  public void testContactCreation() throws Exception {
+  @Test (dataProvider = "validContacts")
+  public void testContactCreation( ContactData contact) throws Exception {
     Contacts before = app.contact().all();
     app.goTo().contactCreation();
-    File photo = new File("src/test/resources/stru.png");
-    ContactData contact = new ContactData()
-            .withFirstName("Name").withLastName("Familiya").withPhoto(photo)
-            .withAddress("Adres").withHomeTel("704").withEmail("@gmail.com").withGroup("[none]");
+
     app.contact().create(contact);
     Contacts after = app.contact().all();
     assertThat(after.size(), equalTo(before.size() + 1));
@@ -29,3 +42,7 @@ public class ContactCreationTests extends TestBase {
 
     }
 }
+//File photo = new File("src/test/resources/stru.png");
+//    ContactData contact = new ContactData()
+//            .withFirstName(firstName).withLastName(lastName).withPhoto(photo)
+//            .withAddress(address).withHomeTel(homeTel).withEmail(email).withGroup(group);
--- addressbook-web-tests/src/test/java/new_package/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/new_package/tests/GroupCreationTests.java
@@ -2,28 +2,37 @@ package new_package.tests;
 
 import new_package.model.GroupData;
 import new_package.model.Groups;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBase {
+  @DataProvider
+  public Iterator<Object[]> validGroups(){
+    List<Object[]> list = new ArrayList<Object[]>();
+    list.add(new Object[] {new GroupData().withGroupName("test 1").withGroupHeader("header 1").withGroupFooter("footer 1")});
+    list.add(new Object[] {new GroupData().withGroupName("test 2").withGroupHeader("header 2").withGroupFooter("footer 2")});
+    list.add(new Object[] {new GroupData().withGroupName("test 3").withGroupHeader("header 3").withGroupFooter("footer 3")});
+    return list.iterator();
+  }
 
+  @Test (dataProvider = "validGroups")
+  public void testGroupCreation(GroupData group) throws Exception {
+      app.goTo().GroupPage();
+      Groups before = app.group().all();
+      app.group().create(group);
+      Groups after = app.group().all();
+      assertThat(app.group().count(), equalTo(before.size()+1));
 
-  @Test
-  public void testGroupCreation() throws Exception {
-    app.goTo().GroupPage();
-    Groups before = app.group().all();
-
-    GroupData group = new GroupData().withGroupName("Test11");
-    app.group().create(group);
-    Groups after = app.group().all();
-    assertThat(app.group().count(), equalTo(before.size()+1));
-
-    assertThat(after, equalTo(
-               before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
-
-    //app.getSessionHelper().logout();
+      assertThat(after, equalTo(
+              before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
+     //app.getSessionHelper().logout();
 
   }
   @Test
@@ -37,9 +46,6 @@ public class GroupCreationTests extends TestBase {
     assertThat(app.group().count(), equalTo(before.size()));
 
     assertThat(after, equalTo(before));
-
-    //app.getSessionHelper().logout();
-
   }
 }
 
