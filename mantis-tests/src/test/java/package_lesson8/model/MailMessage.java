c9de7773574712f6cc4596675b00fd46b59e4ad6
Ruilcaus <beder.alex@gmail.com>
2019-12-12 21:23:17 -0500

    lesson 8.6 failed

---
+++ mantis-tests/src/test/java/package_lesson8/model/MailMessage.java
@@ -0,0 +1,13 @@
+package package_lesson8.model;
+
+public class MailMessage {
+
+  public String to;
+  public String text;
+
+  public MailMessage(String to, String text) {
+    this.to = to;
+    this.text = text;
+  }
+
+}
