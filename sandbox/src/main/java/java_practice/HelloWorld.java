4a9a0b4d6c5c8da2930213c658dd1ea21a48ca92
Ruilcaus <beder.alex@gmail.com>
2019-12-21 20:56:44 -0500

    sandbox rus lang changed to eng

--- sandbox/src/main/java/java_practice/HelloWorld.java
+++ sandbox/src/main/java/java_practice/HelloWorld.java
@@ -6,10 +6,10 @@ public class HelloWorld {
     System.out.println("Hello World");
 
     Square s = new Square(5);
-    System.out.println("Площадь квадрата со сторонами " + s.l + " = " +s.area());
+    System.out.println("Ploshad kvadrata so storonami " + s.l + " = " +s.area());
 
     Rectangle r = new Rectangle(5, 6);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+    System.out.println("Ploshad treugolnika so storonami " + r.a + " и " + r.b + " = " + r.area());
 
   }
 
