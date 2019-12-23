05af3e22eba8c2f109ead6c354585d113ced649c
Ruilcaus <beder.alex@gmail.com>
2019-12-23 14:58:11 -0500

    lesson 10.5

---
+++ addressbook-web-tests/src/test/java/new_package/robotfw/AddressbookKeywords.java
@@ -0,0 +1,33 @@
+package new_package.robotfw;
+
+import new_package.appmanager.ApplicationManager;
+import new_package.model.GroupData;
+import org.openqa.selenium.remote.BrowserType;
+
+import java.io.IOException;
+import java.security.PublicKey;
+
+public class AddressbookKeywords {
+
+  private ApplicationManager app;
+
+  public void initApplicationManager() throws IOException {
+    app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
+    app.init();
+  }
+
+  public void stopApplicationManager(){
+    app.stop();
+    app = null;
+  }
+
+  public int getGroupCount(){
+    app.goTo().GroupPage();
+    return app.group().count();
+  }
+
+  public void createGroup(String name, String header, String footer){
+    app.goTo().GroupPage();
+    app.group().create(new GroupData().withGroupName(name).withGroupHeader(header).withGroupFooter(footer));
+  }
+}
---
+++ addressbook-web-tests/src/test/resources/robotFramework/groups.robot
@@ -0,0 +1,16 @@
+*** Settings ***
+Library  new_package.robotfw.AddressbookKeywords
+
+Suite Setup    Init Application Manager
+Suite Teardown    Stop Application Manager
+
+
+
+
+*** Test Cases ***
+Can Create Group With Valid Data
+    ${old_count} =    Get Group Count
+    Create Group    test name    test header    test footer
+    ${new_count} =    Get Group Count
+    ${expected_count} =    Evaluate    ${old_count} + 1
+    Should Be Equal As Integers    ${new_count}    ${expected_count}
